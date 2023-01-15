6f99ce52e3249fad7000133e7dc42b9acfd55a9c
Arvind0150 <110126393+Arvind0150@users.noreply.github.com>
2023-01-16 00:22:34 +0530

    Update FlipCartPageTest.java

--- InvestmentBanking.maven/src/test/java/com/qa/testLayer/FlipCartPageTest.java
+++ InvestmentBanking.maven/src/test/java/com/qa/testLayer/FlipCartPageTest.java
@@ -1,91 +1,4 @@
-<<<<<<< HEAD
-package com.qa.testLayer;
 
-import java.time.Duration;  
-import java.util.List;
-
-import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
-import org.openqa.selenium.interactions.Actions;
-import org.testng.annotations.Test;
-
-import com.qa.pageLayer.FlipCart;
-import com.qa.testBase.TestBase;
-
-public class FlipCartPageTest extends TestBase {
-	
-		
-		@Test
-		public void verifyFlipCart() throws InterruptedException
-		{
-			
-			
-			
-			
-			Thread.sleep(2000);
-			flip.clickOnClose();
-			
-			Thread.sleep(1000);
-			
-		//	flip.clickOnBeauty();
-			
-			Thread.sleep(1000);
-			
-			flip.clickOnBanner();
-			
-			Thread.sleep(1000);
-			
-			flip.moveToElectronics();
-			
-			Thread.sleep(1000);
-			
-			flip.moveToMobiles();
-
-			
-			
-			
-			Thread.sleep(1000);
-			
-			
-			
-			 List<WebElement> menuItems = driver.findElements(By.xpath("(//div[@class='_1fwVde'])[1]//a[@class='_3QN6WI']"));
-			 
-			// System.out.println(menuItems.size());
-			 int j = 0;
-			 for(int i =0; i<menuItems.size(); i++) 
-			 {
-				 j = i + 1;
-			System.out.println(j + "."+ menuItems.get(i).getText());    
-			
-	        
-			 }
-			 
-			 
-		}
-}
-			 
-			 
-			 
-			 
-
-			
-			
-		
-			
-			
-			
-			
-			
-
-			
-			
-			
-		
-
-
-
-=======
 package com.qa.testLayer;
 
 import java.time.Duration;  
@@ -168,4 +81,4 @@ public class FlipCartPageTest extends TestBase {
 
 
 
->>>>>>> ab4baba20f0628db33c9327d2feed8dd8de6e789
+
